bb328277f0c7e9a6aef1f7e3f6ad14aa978a2cfc
Adeyemo Adekunle <adekunle@nestcoin.com>
2024-10-09 01:57:14 +0100

    feat : AppText

---
+++ lib/src/core/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color primaryColor = const Color(0xFFfcb119);
+  static Color backgroundColor = const Color(0xFFf8f8f7);
+  static Color bodyTextColor = const Color(0xFF9D9281);
+  static Color titleTextColor = const Color(0xFF363333);
+  static Color appBlackColor = const Color(0xFF26252a);
+}
